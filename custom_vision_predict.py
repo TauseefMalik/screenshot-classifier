e611100e409eebcb15c208345e6a183f2703d5e3
Tauseef <tauseefmlk@gmail.com>
2022-02-25 00:28:13 +0530

    Update custom_vision_predict.py

--- custom_vision_predict.py
+++ custom_vision_predict.py
@@ -12,16 +12,25 @@ PROJECT_ID = "<CUSTOM-VISION-PROJECT-ID>"
 def predict(img):
     print("Predicting ...")
     try:
-
         prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": PREDICTION_KEY})
         predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
         
         with open(img, mode="rb") as test_data:
             results = predictor.classify_image(PROJECT_ID, PUBLISH_ITERATION_NAME, test_data.read())
 
+        probability = 0.0
+        probable_tag = ""
         # Display the results.
         for prediction in results.predictions:
+            p = prediction.probability * 100
+            if p > probability:
+                probability = p
+                probable_tag = prediction.tag_name
             print("\t" + prediction.tag_name + ": {0:.2f}%".format(prediction.probability * 100))
+        if probability > 60:
+            print("Probable category: ", probable_tag)
+        else:
+            print("Unable to determine category due to low probability score")
     except Exception as e:
         print(e)
         pass
